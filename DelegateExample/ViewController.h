dc0a8376c96f87dc976e20f3ff110245ffde9293
Loganathan <loganathan_balakrishnan@thbs.com>
2014-03-07 10:22:35 +0530

    Sample project has created

---
+++ DelegateExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DelegateExample
+//
+//  Created by Loganathan on 07/03/14.
+//  Copyright (c) 2014 Loganathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
